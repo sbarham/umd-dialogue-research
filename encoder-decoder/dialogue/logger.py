be04b922b9b580bc73489c601714ddb76f00651b
Samuel Barham <sbarham@terpmail.umd.edu>
2018-03-27 22:47:16 +0000

    See notes from last commit (failed to stage deletions)

--- encoder-decoder/dialogue/logger.py
+++
@@ -1,6 +0,0 @@
-class Logger:
-    def __init__(self, config=Config()):
-        self.LEVEL = config['logging-level']
-
-    def log(self, msg):
-        print("msg") 
--- hred/hred.py
+++
@@ -1,4 +0,0 @@
-"""
-This module encapsulates a Hierarchical Reucrrent Encoder/Decoder (HRED), tailored
-to dialogue processing and, in particular, next-response generation.
-"""
